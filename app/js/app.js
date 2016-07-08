800f9e502a7ca9b00f51bdc6a4b4c3016b7e1653
somethingsaid <kwong.billy@gmail.com>
2016-07-08 13:17:39 -0400

    Tweaking timer

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,8 @@ myApp.controller('GameCtrl', ['$scope', '$timeout', function($scope, $timeout) {
     // Initialize game time at 30 seconds and countdown interval at 1000 milliseconds
     var timeLeft = 30; //
     var downInterval = 1000;
+    var upInterval = [2, 1];
+    var timer;
     $scope.timeLeft = timeLeft;
 
     // Decrement every 1000 milliseconds
@@ -17,13 +19,24 @@ myApp.controller('GameCtrl', ['$scope', '$timeout', function($scope, $timeout) {
             runOut = true;
         }
     };
-    var timer = $timeout($scope.countdown, downInterval);
 
+    // Start timer
+    $scope.startTime =  function() {
+        timer = $timeout($scope.countdown, downInterval);
+    };
+
+    // Add time
     $scope.addTime = function() {
         if ($scope.timeLeft === 0) {
             timer = $timeout($scope.countdown, downInterval);
         };
-        $scope.timeLeft++;
+        $scope.timeLeft += upInterval[0];
+    };
+
+    // Stop time
+    $scope.zeroTime = function() {
+        $timeout.cancel(timer);
+        $scope.timeLeft = 0;
     };
 
     //  Reset
